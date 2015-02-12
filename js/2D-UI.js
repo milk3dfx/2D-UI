59c5161e90be51c03e78b1e5659561ef9c16de25
Sergey Zolotykh <milk3dfx@gmail.com>
2015-02-12 00:18:13 -0500

    Add css classes

--- js/2D-UI.js
+++ js/2D-UI.js
@@ -6,11 +6,12 @@
 if (typeof jQuery === 'undefined') { throw new Error('2D-UI requires jQuery') }
 
 (function($){
+	// UI Context
 	$.fn.UIContext = function(container){
 		this.addClass("ui-context");
 		return this;
 	};
-	
+	// UI Line
 	$.fn.UILine = function(x1,y1, x2, y2){
 		var length = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 		var angle = Math.atan2(y2-y1, x2-x1);
@@ -23,10 +24,18 @@ if (typeof jQuery === 'undefined') { throw new Error('2D-UI requires jQuery') }
 			});
 		return this;
 	};
-	
-	$.fn.UICallout = function(x,y, px, py){
+	// UI Callout
+	$.fn.UICallout = function(x,y, px, py, options){
+
+		var defaultOptions = {
+			base: true,
+			position: "bottom"
+		};
+		options = jQuery.extend(defaultOptions, options);
+
 		var callout = this;
-		callout.addClass("ui-callout");
+		callout.addClass("ui-callout-body").addClass();
+
 		callout.css({
 			"top": y + "px",
 			"left": x + "px"
@@ -34,16 +43,20 @@ if (typeof jQuery === 'undefined') { throw new Error('2D-UI requires jQuery') }
 		setTimeout(function(){
 			var w = callout.outerWidth();
 			var h = callout.outerHeight();
-			$("<div>").UILine(w/2,h-4, w/2,h+20).appendTo(callout);
-			$("<div>").UILine(w/2,h+20,px-x,py-y).appendTo(callout);
+			$("<div>").UILine(w/2,h-4, w/2,h+20).addClass("ui-callout-pointer-base").appendTo(callout);
+			$("<div>").UILine(w/2,h+20,px-x,py-y).addClass("ui-callout-pointer").appendTo(callout);
 		},2);
 		return this;
 	};
+	// UI Path
 	$.fn.UIPath = function(points){
 		if(points.length<2)
 			return this;
+		$(this).addClass("ui-path");
 		for(var i=0; i<points.length-1; i++){
-			$("<div>").UILine(points[i][0],points[i][1],points[i+1][0],points[i+1][1]).appendTo(this);
+			$("<div>").UILine(points[i][0],points[i][1],points[i+1][0],points[i+1][1])
+				.addClass("ui-path-line")
+				.appendTo(this);
 		}
 		return this;
 	};
